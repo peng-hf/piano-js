96fbf565a79317ddcda12f6c07f578f1e84587ca
Philippe Eng <ilyfing@gmail.com>
2018-06-27 01:11:54 -0700

    Add file-loader fot fonts' '

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,16 @@ let config = {
             'sass-loader'
           ]
         })
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: '/fonts/'
+          }
+        }
       }
     ]
   },
